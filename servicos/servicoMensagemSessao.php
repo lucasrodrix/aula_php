3972e5c4d381344e223f278c3475757131704174
Lucas Rodrix <lucasrodrigues1985.lfr@gmail.com>
2021-04-29 19:59:05 -0300

    Organizando parametros nas funções

---
+++ servicos/servicoMensagemSessao.php
@@ -0,0 +1,37 @@
+<?php
+
+session_start();
+
+function setarMensagemErro(string $mensagem):void{
+    $_SESSION['mensagemErro'] = $mensagem;
+}
+
+function setarMensagemSucesso(string $mensagem):void{
+    $_SESSION['mensagemSucesso'] = $mensagem;
+}
+
+function obterMensagemErro():string{
+    if(isset($_SESSION['mensagemErro'])){
+        return $_SESSION['obterMensagemErro'];
+    }
+    return null;
+}
+
+function obterMensagemSucesso():string{
+    if(isset($_SESSION['mensagemSucesso'])){
+        return $_SESSION['obterMensagemSucesso'];
+    }
+    return null;
+}
+
+function removerMensagemErro():void{
+    if(isset($_SESSION['mensagemErro'])){
+        unset($_SESSION['mensagemErro']);
+    }  
+}
+
+function removerMensagemSucesso():void{
+    if(isset($_SESSION['mensagemSucesso'])){
+        unset($_SESSION['mensagemSucesso']);
+    }    
+}
--- servicos/servicoValidacao.php
+++ servicos/servicoValidacao.php
@@ -1,24 +1,24 @@
 <?php
 
-function validaNome(string $nome){
+function validaNome(string $nome):bool{
     if(empty($nome)){
-        $_SESSION['mensagemErro'] = "O nome não pode ser vazio";
+        setarMensagemErro(mensagem: "O nome não pode ser vazio");
         return false;
     }
     elseif(strlen($nome) < 3){
-        $_SESSION['mensagemErro'] = "O nome deve conter mais de 3 caracteres";
+        setarMensagemErro(mensagem: "O nome deve conter mais de 3 caracteres");
         return false;
     }
     elseif(strlen($nome) > 40){
-        $_SESSION['mensagemErro'] = "O nome é muito extenso";
+        setarMensagemErro(mensagem: "O nome é muito extenso");
         return false;
     }
     return true;
 }
 
-function validaIdade(string $idade){
+function validaIdade(string $idade):bool{
     if (!is_numeric($idade)) {
-        $_SESSION['mensagemErro'] = "Informe um número para a idade";
+        setarMensagemErro(mensagem: "Informe um número para a idade");
         return false;
     }
     return true;
