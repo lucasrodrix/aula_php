9638e25a7c05361bbf2296e6ad5cab2804967879
Lucas Rodrix <lucasrodrigues1985.lfr@gmail.com>
2021-04-29 22:36:29 -0300

    Finalizando o script de validação de dados de formulário

--- servicos/servicoCategoriaCompetidor.php
+++ servicos/servicoCategoriaCompetidor.php
@@ -8,6 +8,7 @@ function defineCategoriaCompetidor(string $nome, string $idade):?string{
     $categorias[] = 'adulto';
 
     if(validaNome($nome) && validaIdade($idade)){
+        removerMensagemErro();
         if($idade >= 6 && $idade <= 12){
             for($i = 0; $i < count($categorias); $i++){
                 if ($categorias[$i] == 'infantil'){
@@ -31,6 +32,7 @@ function defineCategoriaCompetidor(string $nome, string $idade):?string{
             }
         }
     }else{
+        removerMensagemSucesso();
         return obterMensagemErro();
     }
 }
